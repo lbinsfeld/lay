f7ca76f396e96ebaace13db5eff532afefe9bbb1
lbinsfeld <160596651+lbinsfeld@users.noreply.github.com>
2024-04-09 08:07:17 -0300

    hgtkuisiy

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+for(let i=0; i<botoes.length; j++){
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[j]
+    }
+}
